8317976154a594abc7939a4d7ea2dc656b4ab46a
khshin <kh.shin@braincommerce.com>
2020-07-10 14:56:37 +0900

    inapp permission 추가

---
+++ app/src/main/java/com/eosr14/masksearch/ui/inapp/InAppActivity.kt
@@ -0,0 +1,14 @@
+package com.eosr14.masksearch.ui.inapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eosr14.masksearch.R
+
+class InAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inapp)
+    }
+
+}
